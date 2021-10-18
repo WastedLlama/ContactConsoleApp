5c48fd6df3ed173fb38726b146481652d21f61c6
WastedLlama <ryanjwilson07@gmail.com>
2021-10-18 22:35:09 +0200

    we start with a phone book app yayyy

---
+++ PhoneBook.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactConsoleApp
+{
+    class PhoneBook
+    {
+        private List<Contact> _contacts { get; set; }
+        private void DisplayContactDetails(Contact contact)
+        {
+            Console.WriteLine($"Contact: {contact.Name}, {contact.Number}");
+        }
+
+        private void DisplayContactDetails(List<Contact> contacts)
+        {
+            foreach (var contact in _contacts)
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+
+        public void AddContact(Contact contact)
+        {
+            _contacts.Add(contact);
+        }
+
+        public void DisplayContact(string number)
+        {
+            var contact = _contacts.FirstOrDefault(c => c.Number == number);
+            if (contact == null)
+            {
+                Console.WriteLine("Contact not found");
+            }
+            else
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+
+        public void DisplayAllContact()
+        {
+            DisplayContactDetails(_contacts);
+            
+        }
+
+        public void DisplayMatchingContact(string searchPhrase)
+        {
+            //name: Ryan
+            //can search by any letter in name?GOOD?BAD?
+            var matchingContacts = _contacts.Where(c => c.Name.Contains(searchPhrase)).ToList();
+            DisplayContactDetails(matchingContacts);
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ContactConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
